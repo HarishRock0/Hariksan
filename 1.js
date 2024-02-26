661251aa137e871ffe3e533427fe2ec68f594563
HarishRock0 <155357398+HarishRock0@users.noreply.github.com>
2024-02-26 14:22:49 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,26 @@
+
+
+function scr1() {
+    // Get the target section by its ID
+    const targetSection = document.getElementById('qf');
+    // Scroll to the top of the target section
+    targetSection.scrollIntoView({ behavior: 'smooth', block:"center" });
+}
+
+function scr2() {
+    // Get the target section by its ID
+    const targetSection = document.getElementById('services');
+
+    // Scroll to the top of the target section
+    targetSection.scrollIntoView({ behavior: 'smooth', block:"center"});
+}
+
+function scr3(){
+    const target = document.getElementById('contact');
+    target.scrollIntoView({behavior: 'smooth', block:"center"});
+}
+
+function scr4(){
+    const target = document.getElementById('about');
+    target.scrollIntoView({behavior: 'smooth', block:"center"});
+}
