51dfe0f83eed937c8da39b4c94482d9c47747494
HarishRock0 <155357398+HarishRock0@users.noreply.github.com>
2024-11-02 10:42:36 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -33,3 +33,21 @@ function scr4(){
     const target = document.getElementById('about');
     target.scrollIntoView({behavior: 'smooth', block:"center"});
 }
+
+
+//<![CDATA[
+(()=>{const e=["Crafting Dreams, One Project at a Time",
+"Your Vision, My Mission",
+"Innovative Solutions for Modern Problems",
+"Turning Ideas into Reality",
+"Expertise You Can Trust,
+"Designing Success Together",
+"Where Creativity Meets Professionalism",
+"Freelance Excellence, Tailored to You""],
+t="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(){}[]<>.,;:",
+n=document.querySelector(".slogan"),o=(e,t,n)=>e.classList[n?"add":"remove"](t),
+a=e=>Math.random()*e|0;let c=0;const r=()=>{o(n,"glitch",0);const r=e[c],
+    s=r.length;let d=0;const l=setInterval(()=>{n.textContent=[...r].map((e,n)=>n<d?e:t[a(t.length)]).join(""),
+    ++d>s&&(clearInterval(l),o(n,"glitch",1),n.dataset.text=n.textContent,c=(c+1)%e.length)},15)},s=()=>{r(),setInterval(r,4e3)};
+/complete|interactive/.test(document.readyState)?setTimeout(s,1):document.addEventListener("DOMContentLoaded",s)})();
+//]]>
